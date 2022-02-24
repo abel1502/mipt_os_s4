7ce65ebaded2475fd84d81dfb01eb05ae26c78dd
abel1502 <terragame.abel@gmail.com>
2022-02-24 19:08:28 +0300

    Implement panic
    Also, __VA_OPT__ is a gcc-unique compiler extension,
    whereas ## __VA_ARGS__ is either common or standard.

--- homeworks/hw1/panic.c
+++ homeworks/hw1/panic.c
@@ -1,9 +1,28 @@
 #include <stdarg.h>
 
 #include "panic.h"
+#include "printk.h"
 
 
+__attribute__((noreturn))
 void __panic(const char* location, const char* msg, ...) {
     // TODO: panic function should print all arguments passed via variadic parameters and then enter inifinite non-busy loop.
     // Example of a such loop could be found in boot.S.
+    va_list args = {};
+    va_start(args, msg);
+    printk("Kernel panic at %s: ", location);
+    if (msg) {
+        vprintk(msg, args);
+    } else {
+        printk("(no message)");
+    }
+    va_end(args);
+
+    asm volatile (
+    "waitloop:\n"
+    "   hlt\n"
+    "   jmp waitloop\n"
+    );
+
+    __builtin_unreachable();
 }
