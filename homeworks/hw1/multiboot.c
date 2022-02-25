570480b352604c3edc7630fd8130bea4181a9b16
abel1502 <terragame.abel@gmail.com>
2022-02-25 23:28:01 +0300

    Implement mutliboot info parsing

--- homeworks/hw1/multiboot.c
+++ homeworks/hw1/multiboot.c
@@ -4,8 +4,51 @@
 #include "panic.h"
 
 
+static struct multiboot_info_header *multiboot_info = NULL;
+uint32_t mutliboot_info_physaddr = 0;
+
+static struct {
+    bool inited;
+    struct multiboot_info_tag_mmap *mmap_tag;
+} multiboot_parsed_info = {.inited = false};
+
+
 void multiboot_init() {
-    // TODO: implement me
+    multiboot_info = (struct multiboot_info_header *)mutliboot_info_physaddr;
+
+    BUG_ON(multiboot_info->total_size < sizeof(struct multiboot_info_tag_end));
+
+    struct multiboot_info_tag *cur_tag = &multiboot_info->tag_storage[0];
+    while (cur_tag->type != MULTIBOOT_TAG_END) {
+        switch (cur_tag->type) {
+        case MULTIBOOT_TAG_MMAP: {
+            struct multiboot_info_tag_mmap *cur_tag_mmap = (struct multiboot_info_tag_mmap *)cur_tag;
+
+            BUG_ON(cur_tag_mmap->entry_size < sizeof(struct multiboot_mmap_entry));
+            BUG_ON(cur_tag_mmap->entry_size % 8 != 0);
+            multiboot_parsed_info.mmap_tag = cur_tag_mmap;
+        } break;
+
+        case MULTIBOOT_TAG_END: {
+            panic("Shouldn't be possible");
+        } break;
+
+        default:
+            break;
+        }
+
+        static_assert(sizeof(struct multiboot_info_tag) == 8, "Bad size");
+
+        u32 cur_size = cur_tag->size;
+        if (cur_size & 0b111) {
+            cur_size = (cur_size & ~0b111) + 8;
+        }
+
+        BUG_ON(cur_size % sizeof(struct multiboot_info_tag) != 0);
+        cur_tag += cur_size / sizeof(struct multiboot_info_tag);
+    }
+
+    multiboot_parsed_info.inited = true;
 }
 
 void multiboot_mmap_iter_init(struct multiboot_mmap_iter *it) {
--- homeworks/hw1/multiboot.h
+++ homeworks/hw1/multiboot.h
@@ -3,8 +3,13 @@
 #define MULTIBOOT_MAGIC    0xE85250D6
 #define MULTIBOOT_ARCH_32  0
 
-#define MULTIBOOT_TAG_END  0
-#define MULTIBOOT_TAG_MMAP 6
+// Currently, only END and MMAP are used
+#define MULTIBOOT_TAG_END       0
+#define MULTIBOOT_TAG_BOOT_CMD  1
+#define MULTIBOOT_TAG_BOOT_NAME 2
+#define MULTIBOOT_TAG_BASIC_MEM 4
+#define MULTIBOOT_TAG_MMAP      6
+#define MULTIBOOT_TAG_FB_INFO   8
 
 #define MULTIBOOT_MMAP_TYPE_RAM  1
 #define MULTIBOOT_MMAP_TYPE_ACPI 3
@@ -15,12 +20,47 @@
 #include "types.h"
 
 
+struct multiboot_info_tag {
+    u32 type;
+    u32 size;
+} __attribute__((packed));
+
+#if 1  // Multiboot tags
+struct multiboot_info_tag_basic_mem {
+    struct multiboot_info_tag header;
+    u32 mem_lower;
+    u32 mem_upper;
+} __attribute__((packed));
+
+struct multiboot_info_tag_mmap {
+    struct multiboot_info_tag header;
+    u32 entry_size;
+    u32 entry_version;
+    u8 entries_storage[];
+} __attribute__((packed));
+
+struct multiboot_info_tag_end {
+    struct multiboot_info_tag header;
+} __attribute__((packed));
+#endif
+
+struct multiboot_info_header {
+    u32 total_size;
+    u32 reserved;
+
+    struct multiboot_info_tag tag_storage[];
+} __attribute__((packed));
+
 struct multiboot_mmap_entry {
     u64 base_addr;
     u64 length;
     u32 type;
     u32 reserved;
 };
+
+
+extern uint32_t multiboot_info_physaddr;
+
 
 // multiboot_init parses information provided by bootloader. It panics, if memory map tag found.
 void multiboot_init();
