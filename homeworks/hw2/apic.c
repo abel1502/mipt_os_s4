e632f1e56a2e393c415973a9cbbb4a8211bf5495
abel1502 <terragame.abel@gmail.com>
2022-03-24 20:21:43 +0300

    Solve hw2 task 3
    Whew, this was exhausting...

--- homeworks/hw2/apic.c
+++ homeworks/hw2/apic.c
@@ -76,7 +76,7 @@ static uint32_t lapic_read(size_t idx) {
 #define TMR_BASEDIV      (1<<20)
 
 static inline void outb(uint16_t port, uint8_t data) {
-    asm volatile("out %0,%1" : : "a" (data), "d" (port));
+    asm volatile ("out %0,%1" : : "a" (data), "d" (port));
 }
 
 #define IOAPIC_REG_TABLE  0x10
@@ -87,46 +87,143 @@ static void ioapic_write(int reg, uint32_t data) {
 }
 
 static void ioapic_enable(int irq, int target_irq) {
-    ioapic_write(IOAPIC_REG_TABLE + 2 * irq, target_irq);
-    ioapic_write(IOAPIC_REG_TABLE + 2 * irq + 1, 0);
+    if (irq < 0) {
+        irq = -irq;
+
+        ioapic_write(IOAPIC_REG_TABLE + 2 * irq + 1, 0);
+        ioapic_write(IOAPIC_REG_TABLE + 2 * irq, target_irq);
+    } else {
+        ioapic_write(IOAPIC_REG_TABLE + 2 * irq + 1, 0);
+        ioapic_write(IOAPIC_REG_TABLE + 2 * irq, target_irq | (1 << 16));
+    }
 }
 
 
-static volatile bool apic_calibration_finished = false;
+static const uint32_t pit_ticks_step = 0x1000;
+static volatile bool apic_calibration_finished = true;
+static volatile uint32_t pit_ticks_remaining = 0;
+
+static inline void pit_set_delay(uint16_t delay) {
+    outb(0x40, (delay     ) & 0xff);
+    outb(0x40, (delay >> 8) & 0xff);
+}
+
+static void pic_eoi() {
+    outb(0x20, 0x20);
+}
+
+static void pic_disable() {
+    outb(0x21, 0xff);
+    outb(0xa1, 0xff);
+}
+
+static void pic_setup() {
+    // Basically a hard-coded initialization
+    outb(0x20, 0x11);
+    outb(0x21, 32);
+    outb(0xa1, 32);
+    outb(0x21, 4);
+    outb(0xa1, 2);
+    outb(0x21, 1);
+    outb(0xa1, 1);
+
+    outb(0x21, 0xff & ~1);
+    outb(0xa1, 0xff);
+}
 
 static void timer_handler_calibration() {
-    printk("Calibrated, woah!\n");
+    // printk("Hit!\n");
+
+    if (apic_calibration_finished) {
+        pic_eoi();
+        return;
+    }
+
+    if (pit_ticks_remaining >= pit_ticks_step) {
+        pit_ticks_remaining -= pit_ticks_step;
+
+        pic_eoi();
+        return;
+    }
+
+    pit_ticks_remaining = 0;
     apic_calibration_finished = true;
+
+    pic_eoi();
 }
 
+// Returns the counter value corresponding to a 1ms apic timer delay
 static uint32_t apic_calibrate_1ms() {
-    const uint32_t counter_initial = 10000000;
+    // Shouldn't run out in the 100ms we're testing it for
+    const uint32_t counter_initial = 0xffffffff;
+    const uint32_t divisor_flag    = 0b1011;
+    const uint32_t divisor_value   = 1;
+    // PIT frequency = 1193182 Hz, so this will constitute 100ms
+    // TODO: This is a lot more than what would fit inside the
+    //       divide counter, so we should use several timer rounds
+    const uint32_t pit_ticks       = 119318;
+    const uint32_t pit_mss         = 100;
 
     timer_handler_t old_handler = set_timer_handler(timer_handler_calibration);
 
     apic_calibration_finished = false;
 
-    // Divisor = 1
-    lapic_write(APIC_TMRDIV, 0b1011);
-    // Interrupt 32, one-shot mode
-    lapic_write(APIC_LVT_TMR, 32 | TMR_ONESHOT);
-    // Counter = a lot
+    // APIC init
+    lapic_write(APIC_TMRDIV, divisor_flag);
+    // Interrupt 39 (spurious), one-shot mode. Should never fire due to the size of the counter
+    lapic_write(APIC_LVT_TMR, 39 | TMR_ONESHOT);
+
+    // PIT init
+    pit_ticks_remaining = pit_ticks;
+    // Channel 0, hi+lo, recurring, binary
+    outb(0x43, 0b00110100);
+
+    // ========================================
+    irq_enable();
+
+    // APIC start
     lapic_write(APIC_TMRINITCNT, counter_initial);
 
-    irq_enable();
+    // PIT start
+    pit_set_delay(pit_ticks_step);
+
+    // Wait for PIT end
     while (!apic_calibration_finished) {
         // Fine since we're waiting for an interrupt
         asm volatile ("hlt");
     }
+
+    // Capture APIC value
+    const uint32_t counter_remaining = lapic_read(APIC_TMRCURRCNT);
+
     irq_disable();
+    // ========================================
+
+    // pit_ticks_remaining should have been exhausted
+    BUG_ON(pit_ticks_remaining > 0);
 
     revert_timer_handler(old_handler);
 
-    return 100000000;
+    const uint32_t counter_delta = counter_initial - counter_remaining;
+
+    // To make sure it isn't unreasonably low
+    BUG_ON(counter_delta < 1000);
+
+    printk("APIC timer works at %u ticks per 1 ms\n", counter_delta * divisor_value / pit_mss);
+
+    return counter_delta * divisor_value / pit_mss;
 }
 
 static void timer_handler_default() {
-    printk(".");
+    static uint32_t counter = 0;
+
+    counter++;
+
+    if (counter >= 1000) {
+        counter = 0;
+
+        printk(".");
+    }
 }
 
 void apic_init() {
@@ -165,8 +262,7 @@ void apic_init() {
     }
 
     // Disable old PIC.
-    outb(0x21, 0xff);
-    outb(0xa1, 0xff);
+    pic_setup();
 
     // Enable APIC, by setting spurious interrupt vector and APIC Software Enabled/Disabled flag.
     lapic_write(APIC_SPURIOUS, 39 | APIC_SW_ENABLE);
@@ -175,7 +271,6 @@ void apic_init() {
     lapic_write(APIC_LVT_PERF, APIC_DISABLE);
 
     // Disable local interrupt pins.
-    lapic_write(APIC_LVT_LINT0, APIC_DISABLE);
     lapic_write(APIC_LVT_LINT1, APIC_DISABLE);
 
     // Signal EOI.
@@ -184,8 +279,14 @@ void apic_init() {
     // Set highest priority for current task.
     lapic_write(APIC_TASKPRIOR, 0);
 
+    // Unmask the PIT and spurious irqs?
+    // ioapic_enable(0, 32 + 0);
+    // ioapic_enable(7, 32 + 7);
+
     // Calibrate APIC timer to fire interrupt every millisecond.
     const uint32_t ticks_1ms = apic_calibrate_1ms();
+
+    pic_disable();
 
     // APIC timer setup.
     // Divide Configuration Registers, set to X1
@@ -197,6 +298,8 @@ void apic_init() {
 
     timer_handler_t old_handler = set_timer_handler(timer_handler_default);
     BUG_ON(old_handler != NULL);
+
+    lapic_write(APIC_LVT_LINT0, APIC_DISABLE);
 }
 
 void apic_eoi() {
